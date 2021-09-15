f4aa0dfd3f2e22dbbf01f871d465ce6f81bf8311
Dasha <shdonly@gmail.com>
2021-09-16 00:13:40 +0300

    add solution count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
@@ -14,7 +12,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(backyard) {
+  let result = backyard.reduce(function (cats, item) {
+    let count = 0;
+    count = 0;
+    item.forEach((item) => {
+      if (item === '^^') {
+        count = count + 1
+      }
+    })
+    return cats + count
+  }, 0);
+  return result
 }
