b4acc98548109dddc7c3b0e53964940533964b08
Dasha <shdonly@gmail.com>
2021-09-21 17:14:16 +0300

    add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,7 +13,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  const { repeatTimes = 0, separator = '+', addition='', additionRepeatTimes = 0, additionSeparator = '|' } = options;
+  console.log(str);
+  console.log(repeatTimes);
+  console.log(separator);
+  console.log(addition);
+  console.log(additionRepeatTimes);
+  console.log(additionSeparator);
+  const additionArr = [];
+  let additionStr = '';
+  if (addition !== '') {
+    additionArr.push(String(addition));
+    while (additionArr.length < additionRepeatTimes) {
+      additionArr.push(String(addition));
+    }
+    additionStr = additionArr.join(additionSeparator);
+  }
+  const resultArr = [str+additionStr];
+  while (resultArr.length < repeatTimes) {
+    resultArr.push(str+additionStr);
+  }
+  const resultStr = resultArr.join(separator);
+  console.log(resultStr);
+  return resultStr;
 }
+
+let case1 = ('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' });
+let case2 = (null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' });
+
+repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' });
